b4ceaca0ca14b825df77a54d458d308ce133495f
sojs-coding <84329771+sojs-coding@users.noreply.github.com>
2022-11-04 00:16:56 +0800

    Authentication for Staff
    Possible to implement for users also.

---
+++ src/controller/Authentication.java
@@ -0,0 +1,82 @@
+package controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ Represents the Authentication service in the application
+ @author Samuel Ong
+ @version 1.0
+ @since 2022-11-03
+ */
+public class Authentication {
+    /**
+     * Map of Username and Password are stored here
+     */
+    private final Map<String, String> userAccounts;
+    /**
+     * Boolean to check if there's an account logged in
+     */
+    private boolean loggedIn = false;
+
+    /**
+     * The constructor to retrieve all usernames and passwords
+     */
+    public Authentication() {
+        userAccounts = new HashMap<>();
+        try {
+            Scanner scStream = new Scanner(new File("./cinemadata/staffacc.txt" ));
+            String inputLine;
+            while (scStream.hasNext())
+            {
+                inputLine = scStream.nextLine();
+                String[] temp = inputLine.split(";");
+                userAccounts.put(temp[0], temp[1]);
+            }
+            scStream.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Staff account file unable to be found!");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("IO error!" + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    /**
+     * To log in as a user
+     * @param username Username for identification
+     * @param password Password for verification
+     * @return whether logged was a success
+     */
+    public boolean login(String username, String password) {
+        if (validateUser(username, password)) {
+            loggedIn = true;
+            return true;
+        }
+        loggedIn = false;
+        return false;
+    }
+
+    /**
+     * To log out
+     */
+    public void logout() {
+        loggedIn = false;
+    }
+
+    /**
+     * To validate the Username and Password
+     * @param username Username
+     * @param password Password
+     * @return Whether the user is valid
+     */
+    private boolean validateUser(String username, String password) {
+        String user = userAccounts.get(username);
+        return user != null;
+    }
+}
