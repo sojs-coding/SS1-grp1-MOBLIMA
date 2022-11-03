5f5446ba0e37855d2fbae9219b01cf708a3136c8
Kyeean <69760467+Kyeean@users.noreply.github.com>
2022-11-04 04:02:13 +0800

    Update MovieReview.java

--- src/entity/movie/MovieReview.java
+++ src/entity/movie/MovieReview.java
@@ -4,5 +4,27 @@ public class MovieReview implements Serializable {
 
 	private String review;
 	private int rating;
+	
+	MovieReview(String review, int rating) {
+		
+		this.review = review;
+		this.rating = rating;
+	}
+	
+	public String getReview(){
+		return review;
+	}
+	
+	public int getRating(){
+		return rating;
+	}
+	
+	public void setReview(String review) {
+		this.review = review;
+	}
+	
+	public void setReview(int rating) {
+		this.rating = rating;
+	}
 
 }
