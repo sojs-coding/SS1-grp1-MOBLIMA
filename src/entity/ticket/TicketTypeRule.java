71bf3f6a8d2470085755d5bd73a5683a89101295
sojs-coding <84329771+sojs-coding@users.noreply.github.com>
2022-11-11 19:22:03 +0800

    Refactored Ticket Type Rules

---
+++ src/entity/ticket/TicketTypeRule.java
@@ -0,0 +1,22 @@
+package entity.ticket;
+
+import entity.booking.PriceRule;
+
+public class TicketTypeRule implements PriceRule {
+    TicketType ticketType;
+
+    public TicketTypeRule(TicketType ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    /**
+     * @param ticket
+     * @return
+     */
+    @Override
+    public boolean isValid(Ticket ticket) {
+        if (ticket.getTicketTypes().contains(ticketType))
+            return true;
+        return false;
+    }
+}
