390134d93b85d330be56da9b42cb9784d6d26703
Kyeean <chinkianhwee@gmail.com>
2022-11-12 21:31:58 +0800

    javadoc

--- src/entity/movie/Review.java
+++ src/entity/movie/Review.java
@@ -6,25 +6,44 @@ public class Review implements Serializable {
 
 	private String review;
 	private int rating;
-	
+
+	/**
+	 * Constructor of Class Review.
+	 * @param review
+	 * @param rating
+	 */
 	public Review(String review, int rating) {
 		
 		this.review = review;
 		this.rating = rating;
 	}
-	
+
+	/**
+	 * Accessor of String variable review
+	 * @return
+	 */
 	public String getReview(){
 		return review;
 	}
-	
+
+	/**
+	 * Accessor of int variable rating
+	 * @return
+	 */
 	public int getRating(){
 		return rating;
 	}
-	
+
+	/**
+	 * Modifier of String variable review
+	 */
 	public void setReview(String review) {
 		this.review = review;
 	}
-	
+
+	/**
+	 * Modifier of int variable rating
+	 */
 	public void setReview(int rating) {
 		this.rating = rating;
 	}
