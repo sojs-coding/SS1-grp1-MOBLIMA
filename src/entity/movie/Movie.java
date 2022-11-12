836b45aa56945ecc5be905ac53bc9a606f476463
Kyeean <chinkianhwee@gmail.com>
2022-11-12 21:28:12 +0800

    javadoc

--- src/entity/movie/Movie.java
+++ src/entity/movie/Movie.java
@@ -1,7 +1,6 @@
 package entity.movie;
 
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Movie implements Serializable {
@@ -26,20 +25,42 @@ public class Movie implements Serializable {
 	private String[] casts;
 	private ArrayList<Review> reviews;
 
+	/**
+	 * Accessor for String variable title
+	 * @return title
+	 */
 	public String getTitle() {
 		return title;
 	}
+
+	/**
+	 * Modifier for String variable title
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
+	/**
+	 * Accessor for String variable synopsis
+	 * @return synopsis
+	 */
 	public String getSynopsis() {
 		return synopsis;
 	}
+
+	/**
+	 * Modifier for String variable synopsis
+	 * @param synopsis
+	 */
 	public void setSynopsis(String synopsis) {
 		this.synopsis = synopsis;
 	}
-	
+
+	/**
+	 * Accessor for enum variable status
+	 * @return status
+	 */
 	public ShowingStatus getStatus() {
 		return status; // Why did I do it this way? Don't go returning a variable in a different form.
 		// They can use ShowingStatus.COMING_SOON. They don't need you to convert it into a String
@@ -56,49 +77,109 @@ public class Movie implements Serializable {
 		}
 		return "Not in switch function.";*/
 	}
+
+	/**
+	 * Modifier for enum variable status
+	 * @param status
+	 */
 	public void setStatus(ShowingStatus status) {
 		this.status = status;
 	}
-	
+
+	/**
+	 * Accessor for double variable overallRating
+	 * @return overallRating
+	 */
 	public double getOverallRating() {
 		return overallRating;
 	}
+
+	/**
+	 * Modifier for double variable overallRating
+	 * @param overallRating
+	 */
 	public void setOverallRating(float overallRating) {
 		this.overallRating = overallRating;
 	}
-	
+
+	/**
+	 * Accessor for enum variable type
+	 * @return type
+	 */
 	public MovieType getType() {
 		return type;
 	}
+
+	/**
+	 * Modifier for enum variable type
+	 * @param type
+	 */
 	public void setType(MovieType type) {
 		this.type = type;
 	}
-	
+
+	/**
+	 * Accessor for String variable director
+	 * @return director
+	 */
 	public String getDirector() {
 		return director;
 	}
+
+	/**
+	 * Modifier for String variable director
+	 * @param director
+	 */
 	public void setDirector(String director) {
 		this.director = director;
 	}
-	
+
+	/**
+	 * Accessor for String Array variable casts
+	 * @return casts
+	 */
 	public String[] getCasts() {
 		return casts;
 	}
+
+	/**
+	 * Modifier for String Array variable casts
+	 * @param casts
+	 */
 	public void setCasts(String[] casts) {
 		this.casts = casts;
 	}
 
+	/**
+	 * Accessor for enum ArrayList variable reviews
+	 * @return reviews
+	 */
 	public ArrayList<Review> getReviews() {
 		return reviews;
 	}
 
+	/**
+	 * Accessor for enum variable in ArrayList of Review with parameter id
+	 * @param id
+	 * @return reviews.get(id)
+	 */
 	public Review getReview(int id) {
 		return reviews.get(id);
 	}
+
+	/**
+	 * Add an enum variable into the ArrayList of Review
+	 * @param review
+	 */
 	public void addReview(Review review) {
 		reviews.add(review);
 	}
-	
+
+	/**
+	 * Set an enum variable into the ArrayList of Review into the parameter id
+	 * @param review
+	 * @param id
+	 */
 	public void setReview(Review review, int id) {
 		reviews.set(id, review);
 	}
